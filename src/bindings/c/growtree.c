eb708ce5769f8c4e7209106f51d5faa90b2f7463
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2020-05-19 11:38:17 +1200

    .c binding file content moved

--- src/bindings/c/growtree.c
+++ src/bindings/c/growtree.c
@@ -4,9 +4,16 @@
 #include "string.h"
 
 void grow_tree_c(int *parent_ne, int *surface_elems, double *angle_max, double *angle_min, double *branch_fraction, double *length_limit, double *shortest_length, double *rotation_limit, int *to_export, const char *filename, int *filename_len);
+void list_mesh_statistics_c(char *filename, int *filename_len);
 
 void grow_tree(int parent_ne, int surface_elems, double angle_max, double angle_min, double branch_fraction, double length_limit, double shortest_length, double rotation_limit,int to_export, const char *filename)
 {
   int filename_len = strlen(filename);
   grow_tree_c(&parent_ne, &surface_elems, &angle_max, &angle_min, &branch_fraction, &length_limit, &shortest_length, &rotation_limit, &to_export, filename, &filename_len);
+}
+
+void list_mesh_statistics(const char *filename)
+{
+  int filename_len = (int)strlen(filename);
+  list_mesh_statistics_c(filename, &filename_len);
 }
